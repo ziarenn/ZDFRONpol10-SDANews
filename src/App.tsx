72f8499dc0f9e409f5361c7fb2ff26a4eac3a2d1
Kacper Żarna <kacper@192.168.1.16>
2022-10-22 14:17:17 +0200

    Kod 22.10.2022 14:15

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import HomePage from "./components/HomePage/HomePage";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <HomePage />
     </div>
   );
 }
---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import { Typography, List } from "@mui/material";
+import { API_KEY } from "../../helpers/helpers";
+const HomePage = () => {
+  // todaysArticles
+  const [todaysArticles, setTodaysArticles] = useState([]);
+
+  // tu sciaganie sa dane z API
+
+  useEffect(() => {
+    const today = new Date();
+    console.log(today);
+    const day = today.getDate(); // getDay => dzień tygodnia (sobota), getDate => dzień miasiąca (21)
+    const month = today.getMonth(); // getMonth => zwraca numer miesiąca (miesiące są indeksowane od zera, styczeń = 0, luty = 1)
+    const year = today.getFullYear(); // getFullYear => rok (2022)
+
+    // 2022-10-22, 2022-01-20
+    const date = `${year}-${month < 9 ? `0${month + 1}` : month + 1}-${
+      day - 1
+    }`;
+    // fetch...
+
+    // cleanup function
+    // return () => {}
+  }, []);
+
+  return (
+    <>
+      <Typography
+        variant="h2"
+        align="center"
+        sx={{ fontSize: "2rem", my: "0.8rem" }}
+      >
+        Today's hottest news:
+      </Typography>
+    </>
+  );
+};
+
+export default HomePage;
